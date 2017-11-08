ada6b361deeed0bfb9e2bade78e65f9ff3481484
Ahmet Tunçel <ahmet.tuncel@outlook.com>
2017-11-08 16:42:52 +0300

    Data type converting
    Data type converting

--- Analyzing_RetailSales.R
+++ Analyzing_RetailSales.R
@@ -1,3 +1,7 @@
+
+
+
+
 # Data Loading 
 features_data_set <- read.csv2("Features data set.csv", header = TRUE, sep = ",")
 sales_data_set <- read.csv2("sales data-set.csv", header = TRUE, sep = ",")
@@ -12,3 +16,17 @@ str(stores_data_set)
 summary(features_data_set)
 summary(sales_data_set)
 summary(stores_data_set)
+
+head(features_data_set, n =2)
+head(sales_data_set, n =2)
+head(stores_data_set, n = 2)
+
+# Data type converting
+
+features_data_set$Date <- as.Date(features_data_set$Date, format = "%d/%m/%y")
+sales_data_set$Date <- as.Date(sales_data_set$Date, format = "%d/%m/%y")
+
+
+
+
+
